b79e3208b53877e866371ea7ebccfe12efc2f89c
Mabi19 <mateuszbartnik33@gmail.com>
2023-07-24 14:34:01 +0200

    Base-demoify fixed point demo

--- components/fixed-point-demo.ts
+++ components/fixed-point-demo.ts
@@ -1,42 +1,22 @@
-import { LitElement, html } from "lit";
-import { customElement, property } from "lit/decorators.js";
-import { bitsToFixedPoint, uintToBits } from "./bits";
-import { bitStyles } from "./styles";
+import { html } from "lit";
+import { customElement } from "lit/decorators.js";
+import { bitsToFixedPoint } from "./bits";
+import { baseDemo } from "./base-demo";
 
 @customElement("fixed-point-demo")
-export class FixedPointDemo extends LitElement {
-
-    @property({ converter: (str) => uintToBits(parseInt(str), 32), type: Array, attribute: "value" })
-    bits: boolean[] = uintToBits(0, 32);
-    static styles = [
-        bitStyles
-    ]
-
-    makeBitElements(bits: boolean[], offset: number = 0, extraClass: string | undefined = undefined) {
-        return bits.map((val, idx) => html`
-            <div class="bit ${extraClass}" @click="${() => this._toggleBit(idx + offset)}">${val ? 1 : 0}</div>
-        `)
+export class FixedPointDemo extends baseDemo({ bits: 32, types: ["fixed-point"] }) {
+    static readonly bitsToNumberFuncs = { 
+        "fixed-point": bitsToFixedPoint
     }
 
-    render() {
-        const numValue = bitsToFixedPoint(this.bits);
+    renderBits() {
         const fractionalPart = this.bits.slice(0, this.bits.length / 2);
         const integerPart = this.bits.slice(this.bits.length / 2);
 
         return html`
-            <div>
-                <div class="bits">
-                    ${this.makeBitElements(fractionalPart, 0, "blue")}
-                    <div class="bit">.</div>
-                    ${this.makeBitElements(integerPart, 16, "red")}
-                </div>
-                value: ${numValue}
-            </div>
-        `;
-    }
-
-    private _toggleBit(idx: number) {
-        this.bits[idx] = !this.bits[idx]
-        this.requestUpdate();
+            ${this.makeBitElements(fractionalPart, { offset: 0, class: "blue" })}
+            <div class="bit">.</div>
+            ${this.makeBitElements(integerPart, { offset: 16, class: "red" })}
+        `
     }
 }
