23c7cd2d4fc0bf2cbf3702b3c60a09522ce5f60b
Mabi19 <mateuszbartnik33@gmail.com>
2023-07-24 21:27:12 +0200

    Base-demoify moving point demo

--- components/moving-point-demo.ts
+++ components/moving-point-demo.ts
@@ -1,33 +1,13 @@
-import { LitElement, html, nothing } from "lit";
-import { customElement, property } from "lit/decorators.js";
+import { html } from "lit";
+import { customElement } from "lit/decorators.js";
 import { when } from "lit/directives/when.js";
-import { bitsToMovingPoint, MOVING_POINT_BITS, uintToBits } from "./bits";
-import { bitStyles } from "./styles";
+import { bitsToMovingPoint, MOVING_POINT_BITS } from "./bits";
+import { baseDemo } from "./base-demo";
 
 @customElement("moving-point-demo")
-export class MovingPointDemo extends LitElement {
-    @property({ converter: (str) => uintToBits(parseInt(str), 32), type: Array, attribute: "value" })
-    bits: boolean[] = uintToBits(0, 32);
-
-    @property({ type: Boolean })
-    hypervalues: boolean = false;
-
-    static styles = [
-        bitStyles
-    ];
-
-    makeBitElements(bits: boolean[], options: Partial<{ offset: number, locked: boolean, extraClass: string}>) {
-        const offset = options.offset ?? 0;
-
-        return bits.map((val, idx) => html`
-            <div
-                class="bit ${options.extraClass} ${options.locked ? 'locked' : undefined}"
-                @click=${!options.locked ? () => this._toggleBit(idx + offset) : nothing}
-            >${val ? 1 : 0}</div>
-        `)
-    }
-
+export class MovingPointDemo extends baseDemo({ bits: 32, types: ["simple", "hypervalues"] }) {
     render() {
+        // due to the bits shifting around we have to just render everything directly
         const { pointPosition, virtualZeroes, value: numValue } = bitsToMovingPoint(this.bits);
 
         const pointPart = this.bits.slice(this.bits.length - MOVING_POINT_BITS);
@@ -36,26 +16,22 @@ export class MovingPointDemo extends LitElement {
 
         console.log(integerPart.length, fractionalPart.length);
 
+
         return html`
             <div>
                 <div class="bits">
-                    ${this.makeBitElements(fractionalPart, { extraClass: "blue" })}
+                    ${this.makeBitElements(fractionalPart, { class: "blue" })}
                     <div class="bit virtual">.</div>
-                    ${this.makeBitElements(new Array(virtualZeroes).fill(false), { locked: true, extraClass: "gray virtual" })}
-                    ${this.makeBitElements(integerPart, { offset: fractionalPart.length, extraClass: "red"})}
-                    ${this.makeBitElements(pointPart, { offset: integerPart.length + fractionalPart.length, extraClass: "green" })}
+                    ${this.makeBitElements(new Array(virtualZeroes).fill(false), { locked: true, class: "gray virtual" })}
+                    ${this.makeBitElements(integerPart, { offset: fractionalPart.length, class: "red"})}
+                    ${this.makeBitElements(pointPart, { offset: integerPart.length + fractionalPart.length, class: "green" })}
                 </div>
                 value:
-                ${when(virtualZeroes == 0 || this.hypervalues,
+                ${when(virtualZeroes == 0 || this.type == "hypervalues",
                     () => html`${numValue}`,
                     () => html`?`
                 )}
             </div>
         `;
-    }
-
-    private _toggleBit(idx: number) {
-        this.bits[idx] = !this.bits[idx]
-        this.requestUpdate();
     }
 }
