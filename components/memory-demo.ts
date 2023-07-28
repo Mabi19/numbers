8df8c9bc92e3ee5ffb43880d1b53a226ec5de2cc
Mabi19 <mateuszbartnik33@gmail.com>
2023-07-28 18:29:05 +0200

    Leave TODO in memory demo

--- components/memory-demo.ts
+++ components/memory-demo.ts
@@ -4,6 +4,7 @@ import { customElement } from "lit/decorators.js";
 @customElement("memory-demo")
 export class MemoryDemo extends LitElement {
     render() {
+        // TODO
         return html`
             <p>memory demo</p>
         `;
