bcc4a2e8e113f1ae6dc3b3f26fe4e6b5ea68bd18
Mabi19 <mateuszbartnik33@gmail.com>
2023-07-29 14:45:12 +0200

    Color top bit red in sign bit demo

--- components/integer-demo.ts
+++ components/integer-demo.ts
@@ -1,6 +1,7 @@
 import { customElement } from "lit/decorators.js";
 import { bitsToInt, bitsToSignBitInt, bitsToUInt } from "./bits";
 import { baseDemo } from "./base-demo";
+import { html } from "lit";
 
 @customElement("integer-demo")
 export class IntegerDemo extends baseDemo({ bits: 8, types: ["unsigned", "twos-complement", "sign-bit", "shifted"] }) {
@@ -9,5 +10,14 @@ export class IntegerDemo extends baseDemo({ bits: 8, types: ["unsigned", "twos-c
         "twos-complement": bitsToInt,
         "sign-bit": bitsToSignBitInt,
         shifted: (bits: boolean[]) => bitsToUInt(bits) - 0x80,
+    }
+
+    renderBits() {
+        // color the top bit if the mode is sign bit
+        const topBitClass = this.type == "sign-bit" ? "red" : undefined;
+        return html`
+            ${this.makeBitElements(this.bits.slice(0, 7))}
+            ${this.makeBitElements(this.bits.slice(7), { class: topBitClass, offset: 7 })}
+        `;
     }
 }
