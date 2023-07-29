02a0a787f53f2439c0939b8a91f0de814ead9b10
Mabi19 <mateuszbartnik33@gmail.com>
2023-07-29 15:29:32 +0200

    Refactor moving point demo
    This version does a little extra computation, but it uses the standard demo functions

--- components/moving-point-demo.ts
+++ components/moving-point-demo.ts
@@ -1,35 +1,41 @@
 import { html } from "lit";
 import { customElement } from "lit/decorators.js";
 import { when } from "lit/directives/when.js";
-import { bitsToMovingPoint, MOVING_POINT_BITS } from "./bits";
+import { bitsToMovingPoint, MOVING_POINT_BITS, splitMovingPoint } from "./bits";
 import { baseDemo } from "./base-demo";
 import { formatGenericNumber } from "./number-formatting";
 
 @customElement("moving-point-demo")
 export class MovingPointDemo extends baseDemo({ bits: 32, types: ["simple", "hypervalues"] }) {
-    render() {
-        // due to the bits shifting around we have to just render everything directly
-        const { pointPosition, virtualZeroes, value: numValue } = bitsToMovingPoint(this.bits);
+    renderBits() {
+        const parts = splitMovingPoint(this.bits);
+
+        // where to draw the point
+        const renderPointPosition = 27 - Math.min(27, parts.pointPosition);
 
         const pointPart = this.bits.slice(this.bits.length - MOVING_POINT_BITS);
-        const integerPart = this.bits.slice(pointPosition, this.bits.length - MOVING_POINT_BITS);
-        const fractionalPart = this.bits.slice(0, pointPosition);
+        const integerPart = this.bits.slice(renderPointPosition, this.bits.length - MOVING_POINT_BITS);
+        const fractionalPart = this.bits.slice(0, renderPointPosition);
 
         return html`
-            <div class="demo">
-                <div class="bits">
-                    ${this.makeBitElements(fractionalPart, { class: "blue" })}
-                    <div class="bit virtual">.</div>
-                    ${this.makeBitElements(new Array(virtualZeroes).fill(false), { locked: true, class: "gray virtual" })}
-                    ${this.makeBitElements(integerPart, { offset: fractionalPart.length, class: "red"})}
-                    ${this.makeBitElements(pointPart, { offset: integerPart.length + fractionalPart.length, class: "green" })}
-                </div>
-                value:
-                ${when(virtualZeroes == 0 || this.type == "hypervalues",
-                    () => html`${formatGenericNumber(numValue)}`,
-                    () => html`?`
-                )}
-            </div>
-        `;
+            ${when(parts.virtualZeroes == 0 || this.type == "hypervalues", () => html`
+                ${this.makeBitElements(fractionalPart, { class: "blue" })}
+                <div class="bit virtual">.</div>
+                ${this.makeBitElements(new Array(parts.virtualZeroes).fill(false), { locked: true, class: "gray virtual" })}
+            `)}
+            ${this.makeBitElements(integerPart, { offset: fractionalPart.length, class: "red"})}
+            ${this.makeBitElements(pointPart, { offset: integerPart.length + fractionalPart.length, class: "green" })}
+        `
+    }
+
+    renderExtra() {
+        const { virtualZeroes, value } = bitsToMovingPoint(splitMovingPoint(this.bits));
+        return html`
+            value:
+            ${when(virtualZeroes == 0 || this.type == "hypervalues",
+                () => html`${formatGenericNumber(value)}`,
+                () => html`?`
+            )}
+        `
     }
 }
