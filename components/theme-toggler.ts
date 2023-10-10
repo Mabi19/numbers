b6dac7eb414078657ae9f5d8ba9febd6882433d2
malay77patra <malay77patra@gmail.com>
2023-10-10 15:12:33 +0530

    theme toggler added

---
+++ components/theme-toggler.ts
@@ -0,0 +1,31 @@
+import { customElement, property } from "lit/decorators.js";
+import { LitElement, css, html } from "lit";
+
+@customElement('theme-toggler')
+export class SimpleNav extends LitElement {
+  @property({ type: Boolean }) isDarkTheme = false;
+
+  static styles = css`
+    button:hover {
+        background-color: lightblue;
+    }
+  `;
+
+  toggleTheme() {
+    this.isDarkTheme = !this.isDarkTheme;
+    document.body.classList.toggle('dark-theme', this.isDarkTheme);
+  }
+
+  render() {
+    return html`
+      <nav>
+        <button @click="${this.toggleTheme}">
+          ${this.isDarkTheme
+            ? "Light"
+            : "Dark"}
+          Toggle Theme
+        </button>
+      </nav>
+    `;
+  }
+}
