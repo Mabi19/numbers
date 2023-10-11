4feb7cab967503d95d855e9adc5aeda98effe54d
malay77patra <malay77patra@gmail.com>
2023-10-11 12:08:54 +0530

    svg added and toggle button added

--- components/theme-toggler.ts
+++ components/theme-toggler.ts
@@ -6,8 +6,42 @@ export class SimpleNav extends LitElement {
   @property({ type: Boolean }) isDarkTheme = false;
 
   static styles = css`
-    button:hover {
-        background-color: lightblue;
+    .switch input {
+      position: absolute;
+      opacity: 0;
+    }
+
+    .switch {
+      display: flex;
+      justify-content: space-between;
+      height: 1em;
+      width: 2em;
+      padding: 1px;
+      background-color: #91a9b1;
+      border: 1px solid #91a9b1;
+      border-radius: 1em;
+      position: relative;
+    }
+    .ball {
+      height: 1em;
+      width: 1em;
+      border-radius: 1em;
+      background: #fff;
+      -webkit-transform: translateX(100%);
+        -moz-transform: translateX(100%);
+              transform: translateX(100%);
+      -webkit-transition: all 300ms;
+        -moz-transition: all 300ms;
+              transition: all 300ms;
+      position: absolute;
+      top: 1px;
+      left: 1px;
+    }
+
+    .switch input:checked + .ball {
+      -webkit-transform: translateX(0%);
+        -moz-transform: translateX(0%);
+              transform: translateX(0%);
     }
   `;
 
@@ -18,12 +52,12 @@ export class SimpleNav extends LitElement {
 
   render() {
     return html`
-      <button @click="${this.toggleTheme}">
-        ${this.isDarkTheme
-          ? "Light"
-          : "Dark"}
-        Mode
-      </button>
+    <label class="switch">
+      <input type="checkbox" onchange="changeTheme()"/>
+      <div class="ball"></div>
+      <img src="moon.svg" />
+      <img src="sun.svg" />
+    </label>
     `;
   }
 }
