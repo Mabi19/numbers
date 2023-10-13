49fb5848e3178ee084a7741b9db4b247ceade670
malay77patra <malay77patra@gmail.com>
2023-10-13 20:19:00 +0530

    light theme component border fixed

--- components/demo-styles.ts
+++ components/demo-styles.ts
@@ -13,7 +13,7 @@ export const demoStyles = css`
     .demo {
         margin: 4px 0;
         padding: 8px;
-        border: 1px solid var(--accent);
+        border: 1px solid var(--border);
         border-radius: 8px;
 
         background-color: var(--secondary);
